96f7302a619a573c24d9961369b9e883ddb10f96
hubenyuan <2017603273@qq.com>
2023-04-14 16:14:11 +0800

    worte path

--- path.c
+++ path.c
@@ -5,27 +5,28 @@
  *       Filename:  path.c
  *    Description:  This file 
  *                 
- *        Version:  1.0.0(04/11/2023)
+ *        Version:  1.0.0(04/13/2023)
  *         Author:  Hu Ben Yuan <2017603273@qq.com>
- *      ChangeLog:  1, Release initial version on "04/11/2023 11:51:05 AM"
+ *      ChangeLog:  1, Release initial version on "04/13/2023 05:41:34 PM"
  *                 
  ********************************************************************************/
 #include <stdio.h>
-#include <libgen.h>
+#include <sys/types.h>
+#include <dirent.h>
 #include <string.h>
-#include <stdlib.h>
-
-static char s_filepath[] = "../w1/device/28-0317320a8aff/";
 
 int main(int argc, char **argv)
 {
-    char *path;
-
-    printf("产品序列号: %s\n",basename(s_filepath));
-
-    free(path);
-    
-    return 0;
+	DIR            *dirptr;
+	struct dirent  *entry;
+	dirptr = opendir("../w1/device/");
+	while( (entry = readdir(dirptr)) != NULL )
+	{
+		if(strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0 )
+		{
+			printf("%s\n", entry->d_name);
+		}
+	}
+	closedir(dirptr);
+	return 0;
 }
-
-
