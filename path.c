950715dfa53094287c5e46ebbb6d1a48fb07c3e6
hubenyuan <2017603273@qq.com>
2023-04-15 18:14:29 +0800

    gain path

--- path.c
+++ path.c
@@ -1,4 +1,4 @@
-/*********************************************************************************
+/********************************************************************************
  *      Copyright:  (C) 2023 Hu Ben Yuan<2017603273@qq.com>
  *                  All rights reserved.
  *
@@ -14,19 +14,35 @@
 #include <sys/types.h>
 #include <dirent.h>
 #include <string.h>
+#include <errno.h>
 
 int main(int argc, char **argv)
 {
+	int             founds = 0;
 	DIR            *dirptr;
 	struct dirent  *entry;
 	dirptr = opendir("../w1/device/");
+
+	if( (entry = readdir(dirptr)) == NULL )
+	{
+		printf("open file failure: %s\n",strerror(errno));
+		return -1;
+	}
+
 	while( (entry = readdir(dirptr)) != NULL )
 	{
-		if(strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0 )
+		if(strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0  && strstr(entry->d_name,"28-") )
 		{
 			printf("%s\n", entry->d_name);
+			founds = 1;
 		}
 	}
 	closedir(dirptr);
+	
+	if( !founds )
+	{
+		printf("can not find ds18b20_path chipest\n");
+		return -2;
+	}
 	return 0;
 }
